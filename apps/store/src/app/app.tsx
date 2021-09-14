ceb20e76e431737da2da2bff9d83c86f0ed08db8
Thor <628364+thor84no@users.noreply.github.com>
2021-09-14 18:54:35 +0100

    Changed store app to use express api.

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,5 +1,4 @@
 import styles from './app.module.scss';
-import { getAllGames } from '../api/fake-api';
 
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -13,6 +12,7 @@ import { Route, Link } from 'react-router-dom';
 
 import { StoreFeatureGameDetail } from '@bg-hoard/store/feature-game-detail';
 import { makeStyles } from '@material-ui/core/styles';
+import { useEffect, useState } from 'react';
 
 const useStyles = makeStyles(() => ({
   cardLink: {
@@ -22,45 +22,78 @@ const useStyles = makeStyles(() => ({
 
 export const App = () => {
   const classes = useStyles();
+  const [state, setState] = useState<{
+    data: any[];
+    loadingState: 'success' | 'error' | 'loading';
+  }>({
+    data: [],
+    loadingState: 'success',
+  });
+
+  useEffect(() => {
+    setState((state) => ({
+      ...state,
+      loadingState: 'loading',
+    }));
+    fetch('/api/games')
+      .then((x) => x.json())
+      .then((res) => {
+        setState((state) => ({
+          ...state,
+          data: res,
+          loadingState: 'success',
+        }));
+      })
+      .catch((err) => {
+        setState((state) => ({
+          ...state,
+          loadingState: 'error',
+        }));
+      });
+  }, []);
 
   return (
     <>
       <Header title={'Board Game Hoard'} />
       <div className={styles.container}>
         <div className={styles['games-layout']}>
-          {getAllGames().map((x) => (
-            <Link to={`/game/${x.id}`} className={classes.cardLink}>
-              <Card key={x.id} className={styles['game-card']}>
-                <CardActionArea>
-                  <CardMedia
-                    className={styles['game-card-media']}
-                    image={x.image}
-                    title={x.name}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {x.name}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      {x.description}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
-                      className={styles['game-rating']}
-                    >
-                      <strong>Rating:</strong> {formatRating(x.rating)}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Link>
-          ))}
+          {state.loadingState === 'loading'
+            ? 'Loading...'
+            : state.loadingState === 'error'
+            ? <div>Error retrieving data</div>
+            : state.data.map((x) => (
+                <Link to={`/game/${x.id}`} className={classes.cardLink}>
+                  <Card key={x.id} className={styles['game-card']}>
+                    <CardActionArea>
+                      <CardMedia
+                        className={styles['game-card-media']}
+                        image={x.image}
+                        title={x.name}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {x.name}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          {x.description}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                          className={styles['game-rating']}
+                        >
+                          <strong>Rating:</strong> {formatRating(x.rating)}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Link>
+              ))}
         </div>
       </div>
 
