cf36fbb039e3df4619f017f8ea95e3daecc8f8b3
Thor <628364+thor84no@users.noreply.github.com>
2021-09-14 21:03:15 +0100

    Fixed storybook typing.

--- libs/store/ui-shared/src/lib/header/header.stories.tsx
+++ libs/store/ui-shared/src/lib/header/header.stories.tsx
@@ -1,13 +1,12 @@
 import { Story, Meta } from '@storybook/react';
-import { Header } from './header';
+import { Header, HeaderProps } from './header';
 
 export default {
   component: Header,
   title: 'Header',
 } as Meta;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const Template: Story<any> = (args) => <Header {...args} />;
+const Template: Story<HeaderProps> = (args) => <Header {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
