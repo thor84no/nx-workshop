d6d9862a6fc5f5c05a2a013273556d29d04a3ff1
Thor <628364+thor84no@users.noreply.github.com>
2021-09-14 20:04:32 +0100

    Removed broken tests.

--- apps/store-ui-shared-e2e/src/integration/store-ui-shared/store-ui-shared.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('store-ui-shared: StoreUiShared component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=storeuishared--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to StoreUiShared!');
-    });
-});
