c20ab9df3bc9982a98998410d26ac65cf882b628
Thor <628364+thor84no@users.noreply.github.com>
2021-09-14 19:16:25 +0100

    Added link to return to root.

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -54,7 +54,7 @@ export const App = () => {
 
   return (
     <>
-      <Header title={'Board Game Hoard'} />
+      <Link className={classes.cardLink} to={'/'}><Header title={'Board Game Hoard'} /></Link>
       <div className={styles.container}>
         <div className={styles['games-layout']}>
           {state.loadingState === 'loading' ? (
