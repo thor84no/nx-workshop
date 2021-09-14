1dd0568fb4071ae2fa7a450a9cc59b6a19401a78
Thor <628364+thor84no@users.noreply.github.com>
2021-09-14 19:09:24 +0100

    Updated store game detail. Fixed key on wrong element.

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -44,7 +44,7 @@ export const App = () => {
           loadingState: 'success',
         }));
       })
-      .catch((err) => {
+      .catch(() => {
         setState((state) => ({
           ...state,
           loadingState: 'error',
@@ -57,43 +57,49 @@ export const App = () => {
       <Header title={'Board Game Hoard'} />
       <div className={styles.container}>
         <div className={styles['games-layout']}>
-          {state.loadingState === 'loading'
-            ? 'Loading...'
-            : state.loadingState === 'error'
-            ? <div>Error retrieving data</div>
-            : state.data.map((x) => (
-                <Link to={`/game/${x.id}`} className={classes.cardLink}>
-                  <Card key={x.id} className={styles['game-card']}>
-                    <CardActionArea>
-                      <CardMedia
-                        className={styles['game-card-media']}
-                        image={x.image}
-                        title={x.name}
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          {x.name}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          color="textSecondary"
-                          component="p"
-                        >
-                          {x.description}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          color="textSecondary"
-                          component="p"
-                          className={styles['game-rating']}
-                        >
-                          <strong>Rating:</strong> {formatRating(x.rating)}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Link>
-              ))}
+          {state.loadingState === 'loading' ? (
+            'Loading...'
+          ) : state.loadingState === 'error' ? (
+            <div>Error retrieving data</div>
+          ) : (
+            state.data.map((x) => (
+              <Link
+                key={x.id}
+                to={`/game/${x.id}`}
+                className={classes.cardLink}
+              >
+                <Card className={styles['game-card']}>
+                  <CardActionArea>
+                    <CardMedia
+                      className={styles['game-card-media']}
+                      image={x.image}
+                      title={x.name}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {x.name}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        {x.description}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                        className={styles['game-rating']}
+                      >
+                        <strong>Rating:</strong> {formatRating(x.rating)}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Link>
+            ))
+          )}
         </div>
       </div>
 
